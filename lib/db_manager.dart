56b3b283c3876dc43e302c72812de286cb7b5d07
Juan Carlos Tinoco <juancarlost496@gmail.com>
2025-02-14 18:10:07 +0100

    vets-UO308831-dart-api-03-Creación del método signUp

---
+++ lib/db_manager.dart
@@ -0,0 +1,71 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
+class DbManager {
+  String _dbName = "vets-dart-api";
+  String _collectionName = "users";
+  late dynamic _collection;
+  late Db _db;
+
+  DbManager(String dbName, String collectionName) {
+    _dbName = dbName;
+    _collectionName = collectionName;
+  }
+
+  DbManager.collection(String collectionName) {
+    _collectionName = collectionName;
+  }
+
+  Future<void> connect() async {
+    final dbUrl =
+        'mongodb+srv://juantinoco:12345@cluster1.gyfsh.mongodb.net/$_dbName';
+
+    _db = await Db.create(dbUrl);
+    await _db.open();
+    _collection = _db.collection(_collectionName);
+    //return _db.collection(_collectionName);
+  }
+
+  Future<void> close() async {
+    await _db.close();
+  }
+
+  Future<List<Map<String, dynamic>>> findAll() async {
+    try {
+      await connect();
+      final data = await _collection.find().toList();
+      return data;
+    } catch (error) {
+      List<Map<String, dynamic>> errorList = [];
+      Map<String, dynamic> error = {
+        "error": "Se ha producido un error al recupera los datos",
+      };
+      errorList.add(error);
+      return errorList;
+    } finally {
+      await close();
+    }
+  }
+
+  Future<dynamic> insertOne(Map<String, dynamic> data) async {
+    try {
+      await connect();
+      final result = await _collection.insertOne(data);
+      if (result.isSuccess) {
+        return {"insertedId": result.id};
+      } else {
+        return {"error": result.writeError.errmsg};
+      }
+    } catch (error) {
+      return {"error": "Se ha produciondo error inesperado"};
+    } finally {
+      await close();
+    }
+  }
+
+  // Method findOne take the same parameter and returns Future of just one map (mongo document) or null if not found
+  Future<dynamic> findOne(filter) async {
+    await connect();
+    final result = await _collection.findOne(filter);
+    return result;
+  }
+}
---
+++ lib/repositories/user_repository.dart
@@ -0,0 +1,23 @@
+import 'package:api/db_manager.dart';
+import 'package:api/encrypt_password.dart';
+import 'package:api/models/users.dart';
+
+class UsersRepository {
+  static DbManager dbManager = DbManager.collection("users");
+  static Future<dynamic> insertOne(User user) async {
+    String encriptedPassword = encryptPassword(user.password);
+    user.password = encriptedPassword;
+    final result = await dbManager.insertOne(user.toJsonInsert());
+    return result;
+  }
+
+  static Future<dynamic> findAll() async {
+    final result = await dbManager.findAll();
+    return result;
+  }
+
+  static Future<dynamic> findOne(Map<String, dynamic> filter) async {
+    final result = await dbManager.findOne(filter);
+    return result;
+  }
+}
